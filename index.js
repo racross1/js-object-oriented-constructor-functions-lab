e6b436c8ed4e16de89e633138f522a93607dc68a
racross1 <racross1@gmail.com>
2021-01-31 12:27:39 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+// class Scooter {
+//     constructor (year, color, model){
+//         this.year = year
+//         this.color = color
+//         this.model = model
+//     }
+// }
+
+function Scooter (year, color, model){
+    this.year = year
+    this.color = color
+    this.model = model
+}
+
+// class Driver {
+//     constructor (name, age, experience) {
+//     this.name = name
+//     this.age = age
+//     this.experience = experience
+// }
+
+function Driver (name, age, experience){
+    this.name = name
+    this.age = age
+    this.experience = experience
+}
+
+// class PickupLocation {
+//     constructor (address, city) {
+//     this.address = address
+//     this.city = city
+// }
+
+function PickupLocation (address, city) {
+    this.address = address
+    this.city = city
+}
